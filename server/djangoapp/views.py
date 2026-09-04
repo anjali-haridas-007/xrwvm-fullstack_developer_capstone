fa53d92811a71d2be73787aef67f73aaa40f0d29
anjali-haridas-007 <myselfanjalihari@gmail.com>
2026-09-04 13:12:40 -0400

    Create Django proxy services for backend APIs

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import login, authenticate, logout
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .restapis import get_request, analyze_review_sentiments, post_review
 # from .populate import initiate
 
 
@@ -98,4 +99,48 @@ def registration(request):
         return JsonResponse({
             "userName": username,
             "status": "Authenticated"
+        })
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/" + state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
+def get_dealer_details(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealer = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealer})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+def get_dealer_reviews(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status": 200})
+        except:
+            return JsonResponse({
+                "status": 401,
+                "message": "Error in posting review"
+            })
+    else:
+        return JsonResponse({
+            "status": 403,
+            "message": "Unauthorized"
         })
